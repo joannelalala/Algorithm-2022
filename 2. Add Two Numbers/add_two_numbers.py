09ab808230ef3c2c13fbabee439c07db6e44b377
jenli810006995 <jc3992@rit.edu>
2022-06-07 08:59:32 -0500

    modified

--- 2. Add Two Numbers/add_two_numbers.py
+++ 2. Add Two Numbers/add_two_numbers.py
@@ -3,32 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from tkinter.tix import ListNoteBook
+
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        c1 = ListNode(l1)
-        c2 = ListNode(l2)
-        
-        sentinel = ListNode(0)
-        d = ListNode(sentinel)
-        total = 0
-        
-        while c1 or c2:
-            total /= 10 
-            # '/=' means 'division assignment', for example, a=10, after referencing a/=5, print(a) gets 2
-            if c1:
-                total += c1.val
-                c1 = c1.next
-            if c2:
-                total += c2.val
-                c2 = c2.next
-            
-            d.next = ListNode(total % 10)
-            d = d.next
-            
-            if total / 10 == 1:
-                d.next = ListNode(1)
-                
-        return sentinel.next
-    
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # create a variable, "carry", to deal with the situation: If the sum vertically exceeds 9, the extra digit "carried" into the next column.
+        # Start from the left-most column
+
+        dummy = cur = ListNode(0)
+        carry = 0
+
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+
+            cur.next = ListNoteBook(carry % 10) # deal with the extra digit
+            cur = cur.next
+            carry //= 10 # division reassignment
+        return dummy.next
+
+# time: O(max(m, n) + 1), m, n stand for l1 & l2. Needs to loop through max(m, n) + 1, 1 is for "carry".
+# space: O(max(m, n) + 1), Need to create an empty list with the size as max(m, n) + 1
+# https://leetcode.com/problems/add-two-numbers/
+
+
     
